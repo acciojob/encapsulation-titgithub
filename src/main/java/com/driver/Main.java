434f8a793704b2ad5cd449af5081c0954167a83a
devashree <devashree2454@gmail.com>
2023-09-15 16:51:40 +0530

    encap changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //   obj.name = "Shubham";
+        //   System.out.println(obj.name);
+        obj.setName("Abhi");
+        System.out.println(obj.getName());
+    }
 }
+  
